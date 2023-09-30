b1ee523c36b84bd0a48e239ec223c4ced5daf5b9
LighghtEeloo <litiaeeloo@gmail.com>
2023-09-29 23:33:09 -0400

    incr: reference interface

--- ref/src/lib.rs
+++ ref/src/lib.rs
@@ -1,59 +1,69 @@
-mod view;
+// use std::collections::HashMap;
 
-use std::collections::HashMap;
+// pub struct Data(Vec<u8>);
 
-use view::FPath;
+// pub enum Node {
+//     File(Data),
+//     Dir(HashMap<String, Type>),
+// }
 
-pub struct Data(Vec<u8>);
+// pub enum Type {
+//     File,
+//     Dir,
+// }
 
-pub enum Node {
-    File(Data),
-    Dir(HashMap<String, Type>),
+// pub struct FileSys {
+//     pub map: HashMap<String, Node>,
+// }
+
+// impl FileSys {
+//     pub fn new() -> Self {
+//         let mut map = HashMap::new();
+//         map.insert("/".to_owned(), Node::Dir(HashMap::new()));
+//         FileSys { map }
+//     }
+// }
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum FileSystemError {
+    #[error("invalid path segment: {0}")]
+    InvalidSegment(String),
 }
 
-pub enum Type {
-    File,
-    Dir,
+pub trait IPath:
+    TryFrom<Self::Raw, Error = FileSystemError> + IntoIterator<Item = Self::Segment> + Sized
+{
+    type Raw;
+    type Segment;
+    type Iter: Iterator<Item = Self::Segment>;
+
+    /* ------------------------------ constructors ------------------------------ */
+    fn root() -> Self;
+    fn append(self, raw_segment: impl AsRef<Self::Raw>) -> Result<Self, FileSystemError>;
+
+    /* ------------------------------- destructors ------------------------------ */
+    fn parent(self) -> Option<(Self, Self::Segment)>;
+    fn iter_parent(&self) -> Self::Iter;
+    fn iter(&self) -> Self::Iter;
 }
 
-pub struct FileSys {
-    pub map: HashMap<String, Node>,
-}
-
-impl FileSys {
-    pub fn new() -> Self {
-        let mut map = HashMap::new();
-        map.insert("/".to_owned(), Node::Dir(HashMap::new()));
-        FileSys { map }
-    }
+pub trait IFileSystem {
+    type Path: IPath;
+    type Data;
 
     /* ----------------------------- file operations ---------------------------- */
-    pub fn create_file(&mut self, path: String) -> anyhow::Result<()>{
-        todo!()
-    }
-    pub fn read_file(&self, path: String) -> anyhow::Result<Data> {
-        todo!()
-    }
-    pub fn write_file(&mut self, path: String, data: Data) -> anyhow::Result<()> {
-        todo!()
-    }
-    pub fn remove_file(&mut self, path: String) -> anyhow::Result<()> {
-        todo!()
-    }
+    fn create_file(&mut self, path: Self::Path) -> Result<(), FileSystemError>;
+    fn read_file(&self, path: Self::Path) -> Result<Self::Data, FileSystemError>;
+    fn write_file(&mut self, path: Self::Path, data: Self::Data) -> Result<(), FileSystemError>;
+    fn remove_file(&mut self, path: Self::Path) -> Result<(), FileSystemError>;
 
     /* -------------------------- directory operations -------------------------- */
-    pub fn create_dir(&mut self, path: String) -> anyhow::Result<()>{
-        todo!()
-    }
-    pub fn read_dir(&self, path: String) -> anyhow::Result<Vec<String>> {
-        todo!()
-    }
-    pub fn remove_dir(&mut self, path: String) -> anyhow::Result<()> {
-        todo!()
-    }
+    fn create_dir(&mut self, path: Self::Path) -> Result<(), FileSystemError>;
+    fn read_dir(&self, path: Self::Path) -> Result<Vec<Self::Path>, FileSystemError>;
+    fn remove_dir(&mut self, path: Self::Path) -> Result<(), FileSystemError>;
 
     /* ----------------------------- link operations ---------------------------- */
-    pub fn create_link(&mut self, path: String, target: String) -> anyhow::Result<()>{
-        todo!()
-    }
+    fn create_link(&mut self, path: Self::Path, target: Self::Path) -> Result<(), FileSystemError>;
 }
